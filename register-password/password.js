d6203d002c8e16036eb9a6066724f504a7452a7d
Omar Muhammad <cds.omarmuhammad92694@alexu.edu.eg>
2025-02-24 20:20:37 +0200

    add the login and regiser

--- register-password/password.js
+++ register-password/password.js
@@ -2,22 +2,17 @@ import { validation } from "../utils/forms.js";
 
 const passwordForm = document.getElementById('password-form');
 
-const customInputsPassword = document.querySelectorAll("my-input");
 
 
 
 passwordForm.onsubmit = function(e) {
     e.preventDefault();
     const formData = new FormData(passwordForm);
-    for (const element of formData) {
-        console.log(element
-        );
-        
-    }
+ 
     
     if (validation.Password(formData)) {
-        if(localStorage.getItem("email") && localStorage.getItem("firstName") && localStorage.getItem("secondName")) {
-            localStorage.clear();
+        if(localStorage.getItem("register-info")){ 
+            localStorage.setItem("register-info","");
         }
         window.location.href = "./register-pass.html";
     } else {
