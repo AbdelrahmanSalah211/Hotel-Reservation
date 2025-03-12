1ca26ba43b05fab5b558c40f865b7d27d161f817
AbdelrahmanEmbaby <73141302+AbdelrahmanEmbaby@users.noreply.github.com>
2025-03-13 00:45:06 +0200

    Finalizing login page and input component

--- components/InputLabel/InputLabel.js
+++ components/InputLabel/InputLabel.js
@@ -90,7 +90,7 @@ class MyInput extends HTMLElement {
 
         return `<div class="my-input-toggle-password" >
         
-            <svg class="my-input-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <svg class="my-input-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" >
                 ${this.isPasswordVisible
                 ? '<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle>'
                 : '<path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line>'}
@@ -130,7 +130,6 @@ class MyInput extends HTMLElement {
 
         container.innerHTML = `
             <label class="my-input-label" for=${this.forLabel || ''}">${this.labelName}</label>
-            ${isPassword ? this.getIcon() : ""}
             <input
                 class="my-input ${isPassword ? "my-input-password" : ""}"
                 name=${this.inputName}
@@ -139,6 +138,7 @@ class MyInput extends HTMLElement {
                 placeholder=" "
                 required
             >
+            ${isPassword ? this.getIcon() : ""}
         `;
 
 
